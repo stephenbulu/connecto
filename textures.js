d5c6dce2abde77322fd6ec28b6f56b372cb9feaa
inferno285 <inferno285@gmail.com>
2018-11-11 03:48:48 -0600

    circles won't connect to another circle already in group

--- textures.js
+++ textures.js
@@ -6,6 +6,16 @@ circleColors = {
     "yellow": ["0xDDDD00", "0xFFFF00"],
     'orange': ["0xFF6000", "0xFF6060"]
 }
+
+circleColors = {
+    "red": ["0x741D11", "0xC3311D"],
+    "blue": ["0x264779", "0x3E74C6"],
+    "purple": ["0x4D2A86", "0x713DC3"],
+    "green": ["0x17814D", "0x1FB46C"],
+    "yellow": ["0x816C04", "0xC3A406"],
+    'orange': ["0x83400B", "0xc66011"]
+}
+
 circleSizes = [15, 25, 40]
 
 shadowAlphas = [0.06, 0.25, 0.35, 0.5]
@@ -27,8 +37,8 @@ Object.keys(circleColors).forEach(function(_color) {
 
         _size = circleSizes[i];
         var graphics = new PIXI.Graphics();
-        graphics.lineStyle(1, 0xFFFFFF, .5);
-        graphics.beginFill(circleColors[_color][0], 1);
+        graphics.lineStyle(0, 0xFFFFFF, .5);
+        graphics.beginFill(circleColors[_color][1], 1);
         graphics.drawCircle(0, 0,_size);
         graphics.endFill();
         circleTextures[_color][_size] = graphics.generateTexture()
@@ -38,7 +48,7 @@ Object.keys(circleColors).forEach(function(_color) {
         for(j=0;j<4;j++){
             var graphics = new PIXI.Graphics();
             graphics.lineStyle(0);
-            graphics.beginFill(circleColors[_color][1], shadowAlphas[j]);
+            graphics.beginFill(circleColors[_color][0], shadowAlphas[j]);
             graphics.drawCircle(0, 0,_size+5+(j*2));
             graphics.endFill();
             shadowTextures[_color][_size].push(graphics.generateTexture())
